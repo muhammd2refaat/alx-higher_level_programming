25c57bf8f6472737c56285141674c53a3ceb8661
muhammd2refaat <mohammed_refat@inbox.ru>
2024-01-22 23:51:02 +0000

    Fix_Errors

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    # Create a copy of my_string
-    new_string = my_string[:]
-    # Loop through the copy of my_string
-    for i in new_string:
-        # Check if i is 'c' or 'C'
-        if i == 'c' or i == 'C':
-            # Remove 'c' or 'C' from the copy of my_string
-            new_string.remove(i)
-    # Return the copy of my_string
+    # Create a new string
+    new_string = ""
+    # Iterate over the characters of the string
+    for char in my_string:
+        # If the character is not 'c' or 'C', append it to the new string
+        if char != 'c' and char != 'C':
+            new_string += char
+    # Return the new string
     return new_string
