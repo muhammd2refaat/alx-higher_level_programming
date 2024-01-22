8ead0b7398ff4d6fa8fa61e74d1fdb0e04fe7dae
muhammd2refaat <mohammed_refat@inbox.ru>
2024-01-22 23:34:04 +0000

    Done_Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    # Check if idx is negative or out of range
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
+    # Replace the element at the specified index
+    new_list = my_list.copy()
+    new_list[idx] = element
+    return new_list
