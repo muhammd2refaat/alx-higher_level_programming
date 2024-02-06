e979606611387dbe58b1c9f3fb1fbfbcc47b4b83
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-06 20:13:14 +0000

    First_Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" square class """
+
+
+class Square:
+    """ square class """
+
+    def __init__(self, size):
+        """init
+        Args
+            size: size of the square
+        """
+        self.__size = size
