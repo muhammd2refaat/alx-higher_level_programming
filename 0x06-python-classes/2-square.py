99f85e8137235a42046a905783ed833e7c330073
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-06 20:27:13 +0000

    2-Square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+class Square:
+    """ square class """
+
+    def __init__(self, size=0):
+        """init
+        Args
+            size: size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ area
+        Returns
+            the current square area
+        """
+
+        return (self.__size * self.__size)
