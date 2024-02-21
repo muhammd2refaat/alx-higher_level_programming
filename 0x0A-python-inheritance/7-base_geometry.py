7415d773dac1aa6501d19e9dd802743a265e1394
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-21 23:59:08 +0000

    Detect_Checker

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""BaseGeometry module"""
+"""Defines a class BaseGeometry based on 6-base_geometry.py"""
 
 
 class BaseGeometry:
-    """BaseGeometry class"""
-
+    """Class BaseGeometry.
+    """
     def area(self):
-        """raises an Exception"""
+        """Area function.
+
+        Raises:
+            Exception: if area is not implemented.
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """check int"""
+        """Validates value.
+
+        Args:
+            name (str): name of the object.
+            value (int): value of the property.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than or equal to 0.
+        """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
+
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
