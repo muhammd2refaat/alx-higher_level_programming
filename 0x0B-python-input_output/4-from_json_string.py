adfc79ec0d502d61e53b6d29986aaa9d8c4ab77f
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-21 16:42:24 +0000

    from-jason-string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
-Write a function that returns the JSON
-representation of an object (string):
-Prototype: def to_json_string(my_obj):
-You don’t need to manage exceptions if
-the object can’t be serialized.
-"""
+"""json string module"""
+
+from json import loads
 
 
-def to_json_string(obj):
-    """Converts an object into its JSON string equivalent."""
-    import json
-    return json.dumps(obj)
+def from_json_string(my_str):
+    """from_json_string
+    this function taks json object and convert it to string
+    arges:
+        my_str: string json obj
+    """
+
+    return loads(my_str)
