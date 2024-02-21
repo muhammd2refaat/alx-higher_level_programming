8ca62a0b3741a2680ad2b44a5bfbc9c059061e1d
muhammd2refaat <mohammed_refat@inbox.ru>
2024-02-21 16:20:05 +0000

    Checker

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key, val in list(a_dictionary.items()):
-        if val == value:
+    for key in list(a_dictionary.keys()):
+        if a_dictionary[key] == value:
             del a_dictionary[key]
     return a_dictionary
