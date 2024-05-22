b46221296410137ae333f0eb48575a2b1ff0e5c1
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-22 04:14:43 +0300

    BAD_CHECKER

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,6 @@
 -- 12. Cheating is bad
 -- this script lists all records with a score of a Bob to 10 in the table second_table
 
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC
+UPDATE second_table
+SET score = 10
+WHERE name = "Bob"
