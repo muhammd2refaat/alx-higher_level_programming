cb516d1da3f53b02321772a8349ae57fd23f0a37
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-22 04:40:13 +0300

    ALTER_TABLE

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,5 @@
 -- 17. Go to UTF8
 -- this script converts hbtn_0c_0 database to UTF8
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+USE hbtn_0c_0
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci
