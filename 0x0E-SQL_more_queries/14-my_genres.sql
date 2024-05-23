540d2a9326de1d5f2c78cd1f43dcaf59926de402
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:35:31 +0300

    ASC

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,7 @@
 -- 14. My genres
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC
-
+SELECT name
+FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id WHERE tv_shows.title = "Dexter"
+GROUP BY name ORDER BY name ASC;
