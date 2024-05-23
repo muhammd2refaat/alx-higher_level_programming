05ddd45ff326c875786a06783145bfb6b3ca8bff
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:17:44 +0300

    AGAIN

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,8 +5,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- Create the user if it does not exist
 -- Set the password of the user to user_0d_2_pwd
-CREATE USER
-    IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant SELECT privilege on hbtn_0d_2.* to user_0d_2@localhost
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
