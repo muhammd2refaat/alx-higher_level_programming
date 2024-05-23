a771a1b10510b08bcd477e1057e49c9a644c4e98
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:22:51 +0300

    SCHOOL

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,11 @@
 -- 7-cities.sql
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states
-ON cities.state_id = states.id
-ORDER BY cities.id
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    PRIMARY KEY(id),
+    id INT AUTO_INCREMENT NOT NULL,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+
+);
