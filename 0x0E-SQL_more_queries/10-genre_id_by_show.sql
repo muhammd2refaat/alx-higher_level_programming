ea90dbb6ce2fee0fb5a45857be1486996ba6df5d
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:28:08 +0300

    CHECKER

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,8 @@
 -- 10. Genre ID by show
 -- lists all genres in the database hbtn_0d_tvshows
 
-SELECT shows.title, genres.id
-FROM tv_shows AS shows
-INNER JOIN tv_show_genres AS show_genres
-ON shows.id = show_genres.show_id
-ORDER BY shows.title, genres.id
+SELECT s.title, g.genre_id
+  FROM tv_shows AS s
+        INNER JOIN tv_show_genres AS g
+	ON s.id = g.show_id
+ ORDER BY s.title, g.genre_id;
