8b9ba7d9c4d3ef26d7af1db70d7be2b137831185
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:14:44 +0300

    TEST_ERRORS

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,6 @@
 -- 2. Read user
 -- this script creates the MySQL user user_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost'
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
