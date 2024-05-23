b8004fce46f353db2b64f488f4650c7c3383cb85
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 03:52:14 +0300

    never_NULL

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,5 @@
+-- 4. ID can't be null
+-- this script creates the table id_not_null 
+-- id INT with the default value 1, name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256))
