67e29f40dde1cfb349b509ef5be632e5eb46721f
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 05:10:37 +0300

    test

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -10,4 +10,4 @@ ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name IS NULL
-ORDER BY tv_shows.title, tv_genres.name
+ORDER BY tv_shows.title, tv_genres.name ASC;
