7dc2a4d863238edd270f867208a7678354191235
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 05:05:13 +0300

    changed

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,8 +2,8 @@
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 
 SELECT shows.title, genres.genre_id
-FROM tv_shows AS shows
-LEFT JOIN tv_show_genres AS genres
-ON tv_shows.id = genres.show_id
-WHERE genres.genre_id IS NULL
-ORDER BY shows.title, genres.genre_id;
+  FROM tv_shows AS shows
+       LEFT JOIN tv_show_genres AS genres
+       ON shows.id = genres.show_id
+       WHERE genres.genre_id IS NULL
+ ORDER BY shows.title, genres.genre_id;
