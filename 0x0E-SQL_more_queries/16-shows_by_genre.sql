ce13acaf74d23538b2add2c2dbb0d6157cbd29e1
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 04:37:23 +0300

    MANDATORY

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,7 @@
 -- 16. List shows and genres
 -- lists all shows contained in the database hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title
+  FROM tv_show_genres 
+    JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_genres.name;
