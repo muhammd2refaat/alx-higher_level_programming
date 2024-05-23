151809b6938df4ae8d583085288e6ffdbd67d927
muhammd2refaat <mohammed_refat@inbox.ru>
2024-05-23 05:07:48 +0300

    no_comedy_tonight

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+-- 18. No Comedy tonight!
+-- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title
