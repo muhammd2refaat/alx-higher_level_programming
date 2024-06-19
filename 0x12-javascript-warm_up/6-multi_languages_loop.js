2ad89191a143a0b78dc30faaefded4f328ef327c
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-19 05:45:57 +0300

    done

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
-
-lang = [
-  'C is fun',
-  'Python is cool',
-  'JavaScript is amazing'
-];
-
-for (let i = 0; i < lang.length; i++) {
-  console.log(lang[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
