33fed9cd14c4da6e1aaff75ec2ded4b02b3e52c4
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-19 05:54:26 +0300

    Done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < args[0]; i++) {
-    console.log('X'.repeat(args[0]));
-  }// repeat() is used to repeat a string
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
