80e101f590e1566320283d0fd0c26d215e3a7e22
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-19 05:51:17 +0300

    sit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-x = 'C is fun';
+X = 'C is fun';
 
 if (process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < process.argv[2]; i++) {
-    console.log(x);
+    console.log(X);
   }
 }
