4530f3836dde0e04fb2aa4161330bb80e179c75d
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-20 03:51:58 +0300

    feat: Export Rectangle class for use in other files

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -2,5 +2,5 @@
 
 // Create an empty class Rectangle that defines a rectangle:
 class Rectangle {}
-
+// module.exports exports the class Rectangle to be used in other files
 module.exports = Rectangle;
