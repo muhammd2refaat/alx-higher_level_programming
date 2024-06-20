f842dbdb702f609cec003cb218b9a7c505617095
muhammd2refaat <mohammed_refat@inbox.ru>
2024-06-20 04:03:31 +0300

    feat: Initialize Rectangle class with width and height attributes
    This commit initializes the Rectangle class by adding a constructor that takes in two arguments, `w` and `h`, and assigns them to the instance attributes `width` and `height` respectively. This allows for the creation of Rectangle objects with specific dimensions.

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,6 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
+    // constructor receives two arguments w and h, and initializes the instance attributes width and height
+    // this refers to the current object
     this.width = w;
     this.height = h;
   }
