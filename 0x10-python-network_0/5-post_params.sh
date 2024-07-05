3248e8142bbde2def0e9f477e0e972b4add7897f
muhammd2refaat <mohammed_refat@inbox.ru>
2024-07-05 03:57:08 +0300

    Refactor 0x10-python-network_0/5-post_params.sh to remove duplicate code and improve readability

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will send a POST request to the URL passed as the first argument and display the body of the response
-curl -s -X POST "$1" -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
