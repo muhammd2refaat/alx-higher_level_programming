6f0947381d9eb32fd4903cfe1d5292f777d79e4f
muhammd2refaat <mohammed_refat@inbox.ru>
2024-07-05 03:57:44 +0300

    Refactor 0x10-python-network_0/5-post_params.sh to use consistent line endings

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script will send a POST request to the URL passed as the first argument and display the body of the response
-curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
