3d4013100a4b3f5725a28071223ae7fc0b038577
muhammd2refaat <mohammed_refat@inbox.ru>
2024-07-05 03:45:32 +0300

    Refactor find_peak function to use binary search for improved efficiency

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python3
+"""
+This module defines a function that finds
+a peak in a list of unsorted integers.
+"""
 
-def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers"""
-    if not list_of_integers:
+
+def find_peak(num_list):
+    """Find the peak in a list of integers.
+    Args:
+        num_list (list): List of integers.
+    Returns:
+        int or None: The value of the peak or None if the list is empty.
+    """
+    lst_len = len(num_list)
+    if lst_len == 0:
         return None
-    n = len(list_of_integers)
-    if n <= 3:
-        return max(list_of_integers)
-    for i in range(1, n - 1):
-        if (
-            list_of_integers[i] > list_of_integers[i - 1] and
-            list_of_integers[i] > list_of_integers[i + 1]
-        ):
-            return list_of_integers[i]
-    return max(list_of_integers)
+    peak_index = binary_search(num_list, 0, lst_len - 1)
+    return num_list[peak_index]
+
+
+def binary_search(nums, low, high):
+    """Recursive binary search of the peak.
+    Args:
+        nums (list): List of integers.
+        low (int): Lower bound of the search range.
+        high (int): Upper bound of the search range.
+    Returns:
+        int: Index of the peak.
+    """
+    if low >= high:
+        return low
+
+    mid = (high - low) // 2 + low
+    if nums[mid] > nums[mid + 1]:
+        return binary_search(nums, low, mid)
+    else:
+        return binary_search(nums, mid + 1, high)
